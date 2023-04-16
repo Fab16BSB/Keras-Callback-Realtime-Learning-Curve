81be617376eb8eae2645a2b608fc055f974dd295
Fab.16 <fab_16@hotmail.fr>
2023-04-16 13:53:57 +0200

    Actual version

--- callback.py
+++ callback.py
@@ -1,87 +1,77 @@
 import os
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-import pandas as pd
-import csv
 from keras.callbacks import Callback
 
-
 class LearningCurves(Callback):
-    def __init__(self, save_path="", graph_name="LearningCurve.png", save_graph=True, show_graph=False, black_background=False, csv_to_load=""):
 
-        self.save_path = save_path
-        self.graphe_name = graph_name
+    def __init__(self, savePath="", fileName="LearningCurve.png", lastEpoch=0, save_graph=True, show_graph=True, blackBackground=False):
+        self.savePath = savePath
+        self.fileName = fileName
         self.save_graph = save_graph
         self.show_graph = show_graph
-        
+        self.lastEpoch = lastEpoch
 
-        self.data = {
-            'epoch': [],
-            'accuracy': [],
-            'loss': [],
-            'val_accuracy': [],
-            'val_loss': []
-        }
-
-        # Charger les données d'un fichier CSV existant
-        if csv_to_load != "" and os.path.exists(csv_to_load):
-            self.__read_csv(csv_to_load)
-           
-        '''
         # Override graphe si nécessaire
-        if self.csv_to_load != "" and os.path.exists(self.csv_to_load) and os.path.exists(os.path.join(save_path, graph_name)):
+        if self.lastEpoch == 0 and os.path.exists(os.path.join(savePath, fileName)):
+            print("remove graphe")
             os.remove(os.path.join(savePath, fileName))
-        '''
 
         # Configuration du graphe
-        if black_background:
+
+        # Set background
+        if blackBackground == True :
             plt.style.use('dark_background')
-        else:
+        else :
             plt.style.use('classic')
 
-
+        # Séparation du graphe en 2 section : loss / accuracy
         self.figure, self.axs = plt.subplots(2)
         self.figure.suptitle('Learning Curves')
         self.axs[0].set_ylabel("Loss")
         self.axs[1].set_ylabel("Accuracy")
 
+        # Configuration de la structure pour stockage des données
+        self.data = {
+            'epoch': [],
+            'loss': {'train': [], 'val': []},
+            'accuracy': {'train': [], 'val': []}
+        }
+
+
+    # À chaque tour du training (train + validation)
     def on_epoch_end(self, epoch, logs={}):
+
+        # On change le numéro d'epoch si lastEpoch défini
+        epoch = epoch + self.lastEpoch
+
+        # Stockage des performance de l'epoch actuel
         self.data['epoch'].append(epoch)
-        self.data['accuracy'].append(logs.get('accuracy'))
-        self.data['loss'].append(logs.get('loss'))
-        self.data['val_accuracy'].append(logs.get('val_accuracy'))
-        self.data['val_loss'].append(logs.get('val_loss'))
+        self.data['loss']['train'].append(logs.get('loss'))
+        self.data['accuracy']['train'].append(logs.get('accuracy'))
 
-        self.axs[0].plot(self.data['epoch'], self.data['loss'], color='r')
-        self.axs[1].plot(self.data['epoch'], self.data['accuracy'], color='r')
+        # Plot Accuracy / Loss performance
+        self.axs[0].plot(self.data['epoch'], self.data['loss']['train'], color='r')
+        self.axs[1].plot(self.data['epoch'], self.data['accuracy']['train'], color='r')
 
+        # Si validation dispo plot validation accuracy / loss performance
         if 'val_loss' in logs:
-            self.axs[0].plot(self.data['epoch'], self.data['val_loss'], color='b')
-            self.axs[1].plot(self.data['epoch'], self.data['val_accuracy'], color='b')
+            self.data['loss']['val'].append(logs.get('val_loss'))
+            self.data['accuracy']['val'].append(logs.get('val_accuracy'))
 
+            self.axs[1].plot(self.data['epoch'], self.data['accuracy']['val'], color='b')
+            self.axs[0].plot(self.data['epoch'], self.data['loss']['val'], color='b')
+
+        # Ajout de la légende
         red_patch = mpatches.Patch(color='red', label='Train')
         blue_patch = mpatches.Patch(color='blue', label='Validation')
         self.axs[0].legend(handles=[red_patch, blue_patch], loc=1)
         self.axs[1].legend(handles=[red_patch, blue_patch], loc=4)
 
-        if self.show_graph:
+        # Affichage du graphe
+        if self.show_graph == True:
             plt.show()
 
-        if self.save_graph:
-            self.figure.savefig(os.path.join(self.save_path, self.graphe_name))
-
-
-    def __read_csv(self, path_to_csv):
-        with open(path_to_csv, 'r') as f:
-            reader = csv.DictReader(f)
-
-            for row in reader:
-                self.data['epoch'].append(int(row['epoch']))
-                self.data['accuracy'].append(float(row['accuracy']))
-                self.data['loss'].append(float(row['loss']))
-
-                if 'val_accuracy' in row:
-                    self.data['val_accuracy'].append(float(row['val_accuracy']))
-
-                if 'val_loss' in row:
-                    self.data['val_loss'].append(float(row['val_loss']))
+        # Sauvegarde du graphe
+        if self.save_graph == True:
+            self.figure.savefig(os.path.join(self.savePath, self.fileName))
