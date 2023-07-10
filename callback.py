d04c9a38b76d2ac11c5d07965ffdb8fcdaa70824
Fabien Poirier (Fab.16) <fab_16@hotmail.fr>
2023-07-10 16:43:39 +0200

    Learning Curve V2
    Le fichier "first_reseau_de_neuronne.py" c'est notre code beta du diabete je l'ai utilisé pour testé mes courbes.
    Concernant le callback beaucoup de changement actuellement le code n'est pas clean, ni même fini et tout n'a pas été testé mais normalement il est possible de générer un graphe par metrique avec 2 courbes a chaque fois train / validation pour n'importe quel metrique loss, accuracy ou des metrique perso.

--- callback.py
+++ callback.py
@@ -1,79 +1,209 @@
 import os
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import numpy as np
+from datetime import datetime
 from keras.callbacks import Callback
+from sklearn.metrics import f1_score, precision_score, recall_score
+import keras.backend as K
+
+
 
 class LearningCurves(Callback):
 
-    def __init__(self, savePath="", fileName="LearningCurve.png", lastEpoch=0, save_graph=True, show_graph=True, blackBackground=False):
+    #TODO revoir commentaire
+    def __init__(self, savePath=".", show_graph=True, save_graph=True, override=True, blackBackground=False,  metric=["Loss"]):
+        '''
+        Objectif :
+        Params :
+        '''
+
         self.savePath = savePath
-        self.fileName = fileName
-        self.save_graph = save_graph
         self.show_graph = show_graph
-        self.lastEpoch = lastEpoch
+        self.save_graph = save_graph
+        self.metric = metric
 
-        # Override graphe si nécessaire
-        if self.lastEpoch == 0 and os.path.exists(os.path.join(savePath, fileName)):
-            print("remove graphe")
-            os.remove(os.path.join(savePath, fileName))
-
-        # Configuration du graphe
+        self.keyList = []
+        self.figures = []
+        self.axes = []
+        self.data = {}
 
         # Set background
+        self.setBackground(blackBackground)
+
+        # Activer le mode interactif
+        plt.ion()  
+
+        # Create path if not exist and if graphe exist and override false add datetime on graph name
+        self.extension = self.checkPath(override)
+
+        # Information to the User
+        print("\n[INFO] Des graphiques lié a la precision, recall ou f1-score peuvent etre généré pour cela ajouter les methodes statique : precision, recall et f1_score dans l'attribut metrique de votre methode compile.")
+        print("Exemple : model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', LearningCurves.precision, LearningCurves.recall, LearningCurves.f1_score])\n")
+        
+
+        
+
+    #TODO revoir commentaire
+    # À chaque tour du training (train + validation)
+    def on_epoch_end(self, epoch, logs={}):
+        
+        assert "val_loss" in logs, "[ERROR] Pas de validation set trouver"
+
+
+        # Si c'est la premiere epoch
+        if 'epoch' not in self.data:
+
+            # Preparation structure de données
+            self.data['epoch'] = []
+
+            for key in logs :
+                self.data[key] = []
+
+
+            # Filtrage des metric
+            keys = list(self.data.keys())
+            self.keyList = [key.lower() for key in keys if key != "epoch" and not key.startswith('val_')]
+            self.metric = self.keyList if self.metric == "*" else [key.lower() for key in self.metric if key.lower() in self.keyList]
+
+            # Preparation du graphe
+            self.setGraphe(self.metric)
+
+
+        # Stockage des performance de l'epoch actuel
+        for key in logs :
+            self.data[key].append(logs[key])
+
+        self.data['epoch'].append(epoch)
+
+        
+        # Desin sur le graphe
+        self.plotGraphe(self.metric)
+
+        
+        # Sauvegarde du graphe
+        self.saveGraphe()
+                
+
+        # Affichage du graphe    
+        self.showGraphe(0.1)
+            
+        
+    #TODO revoir commentaire
+    def on_train_end(self, logs=None):
+        plt.close()
+        metrics = list(set(self.keyList) - set(self.metric))
+        self.setGraphe(metrics, False)
+        self.plotGraphe(metrics)
+        self.saveGraphe()
+
+
+    #TODO revoir commentaire
+    def setGraphe(self, listeMetric, show=True):
+
+        # Création des graphes individuels
+        for key in self.data:
+            if key != "epoch" and key in listeMetric:
+
+                # Creation des figures
+                fig = plt.figure()
+                fig.suptitle('Learning Curves - ' + key + ' Graph')
+
+                # Ajout de la légende
+                red_patch = mpatches.Patch(color='red', label='Train')
+                blue_patch = mpatches.Patch(color='blue', label='Validation')
+                fig.legend(handles=[red_patch, blue_patch], loc=1)
+                self.figures.append(fig)
+
+                # Creation des axes
+                ax = fig.add_subplot()
+                ax.set_xlabel('Epoch')
+                ax.set_ylabel('Value')
+                self.axes.append(ax)
+
+                # Empèche les graphes d'etre affiché
+                if show == False:
+                    matplotlib.use('agg')
+
+
+    #TODO revoir commentaire
+    def plotGraphe(self, metricList):
+        for index in range(0, len(metricList)) :
+            key = self.keyList[index]
+            ax = self.axes[index]
+            
+            ax.plot(self.data['epoch'], self.data[key], color='r')
+            ax.plot(self.data['epoch'], self.data['val_'+key], color='b')
+
+
+    #TODO revoir commentaire
+    def showGraphe(self, time):
+        if self.show_graph == True:
+            plt.show()
+
+            # Ajouter une pause entre les mises à jour
+            plt.pause(time)
+
+
+
+    #TODO revoir commentaire
+    def saveGraphe(self):
+        if self.save_graph == True:
+
+            for indexFigure in range(0, len(self.figures)) :
+                print(indexFigure, self.keyList[indexFigure])
+                path = os.path.join(self.savePath, self.keyList[indexFigure] + self.extension)
+
+                self.figures[indexFigure].savefig(path)
+
+
+    #TODO revoir commentaire
+    def setBackground(self, blackBackground):
         if blackBackground == True :
             plt.style.use('dark_background')
         else :
-            plt.style.use('classic')
-
-        # Séparation du graphe en 2 section : loss / accuracy
-        self.figure, self.axs = plt.subplots(2)
-        self.figure.suptitle('Learning Curves')
-        self.axs[0].set_ylabel("Loss")
-        self.axs[1].set_ylabel("Accuracy")
-
-        # Configuration de la structure pour stockage des données
-        self.data = {
-            'epoch': [],
-            'loss': {'train': [], 'val': []},
-            'accuracy': {'train': [], 'val': []}
-        }
+            plt.style.use('classic') 
 
 
-    # À chaque tour du training (train + validation)
-    def on_epoch_end(self, epoch, logs={}):
 
-        # On change le numéro d'epoch si lastEpoch défini
-        epoch = epoch + self.lastEpoch
+    #TODO revoir commentaire  
+    def checkPath(self, override):
+        extension = ".png"
 
-        # Stockage des performance de l'epoch actuel
-        self.data['epoch'].append(epoch)
-        self.data['loss']['train'].append(logs.get('loss'))
-        self.data['accuracy']['train'].append(logs.get('accuracy'))
+        if not os.path.exists(self.savePath):
+            os.makedirs(self.savePath)
 
-        # Plot Accuracy / Loss performance
-        self.axs[0].plot(self.data['epoch'], self.data['loss']['train'], color='r')
-        self.axs[1].plot(self.data['epoch'], self.data['accuracy']['train'], color='r')
+        else:
+            if override == False and os.path.exists(os.path.join(self.savePath, "loss.png")): 
+                current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                extension = "_" + current_time + ".png"
 
-        # Si validation dispo plot validation accuracy / loss performance
-        if 'val_loss' in logs:
-            self.data['loss']['val'].append(logs.get('val_loss'))
-            self.data['accuracy']['val'].append(logs.get('val_accuracy'))
+        return extension
 
-            self.axs[1].plot(self.data['epoch'], self.data['accuracy']['val'], color='b')
-            self.axs[0].plot(self.data['epoch'], self.data['loss']['val'], color='b')
 
-        # Ajout de la légende
-        red_patch = mpatches.Patch(color='red', label='Train')
-        blue_patch = mpatches.Patch(color='blue', label='Validation')
-        self.axs[0].legend(handles=[red_patch, blue_patch], loc=1)
-        self.axs[1].legend(handles=[red_patch, blue_patch], loc=4)
 
-        # Affichage du graphe
-        if self.show_graph == True:
-            #plt.show()
-            plt.draw()
-            plt.pause(0.001)
+    # TODO A COMMENTER
+    # Fonctions de métriques personnalisées
+    # https://aakashgoel12.medium.com/how-to-add-user-defined-function-get-f1-score-in-keras-metrics-3013f979ce0d
+    @staticmethod
+    def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
 
-        # Sauvegarde du graphe
-        if self.save_graph == True:
-            self.figure.savefig(os.path.join(self.savePath, self.fileName))
+    @staticmethod
+    def recall(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    @staticmethod
+    def f1_score(y_true, y_pred):
+        precision_val = LearningCurves.precision(y_true, y_pred)
+        recall_val = LearningCurves.recall(y_true, y_pred)
+        f1_val = 2 * ((precision_val * recall_val) / (precision_val + recall_val + K.epsilon()))
+        return f1_val
+            
